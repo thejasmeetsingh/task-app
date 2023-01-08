bef4b610fc2f74307213d780b93a9c455fdb87e2
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2023-01-08 14:14:18 +0530

    Reformat the file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,35 +2,3 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://127.0.0.1:27017/task-app", { useNewUrlParser: true })
-
-
-// async function createTaskModel() {
-//     await mongoose.connect("mongodb://127.0.0.1:27017/task-app", { useNewUrlParser: true })
-
-//     const TaskSchema = new mongoose.Schema({
-//         description: {
-//             type: String,
-//             required: true,
-//             trim: true
-//         },
-//         completed: {
-//             type: Boolean,
-//             default: false
-//         }
-//     })
-
-//     const Task = mongoose.model("Task", TaskSchema)
-
-//     const task = new Task({
-//         description: " Lorem Ipsum",
-//         // completed: true
-//     })
-    
-//     await task.save().then((res) => {
-//         console.log("Result:", res);
-//     }).catch((error) => {
-//         console.log("Error:", error);
-//     })
-// }
-
-// createTaskModel()
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-const express = require("express");
 require("./db/mongoose");
-
+const express = require("express");
 const User = require("./models/user")
 const Task = require("./models/task")
 
@@ -10,6 +9,7 @@ const port  = process.env.PORT || 3000
 
 
 app.use(express.json());
+
 
 app.post("/user/", (req, res) => {
     const user = new User(req.body)
@@ -30,6 +30,7 @@ app.post("/task/", (req, res) => {
         res.status(400).send(error)
     })
 })
+
 
 app.listen(port, () => { 
     console.log(`Server is up at port ${port}`) 
