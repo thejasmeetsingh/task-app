ffcdd11e8b451b455c058746f0b730a74829e533
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2023-01-08 14:22:29 +0530

    Added Users and Tasks List API and updated the status codes

--- src/index.js
+++ src/index.js
@@ -15,9 +15,17 @@ app.post("/user/", (req, res) => {
     const user = new User(req.body)
     
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+app.get("/user/", (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send(error)
     })
 })
 
@@ -25,9 +33,17 @@ app.post("/task/", (req, res) => {
     const task = new Task(req.body)
 
     task.save().then(() => {
-        res.send(task)
+        res.status(201).send(task)
     }).catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+app.get("/task/", (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(500).send(error)
     })
 })
 
